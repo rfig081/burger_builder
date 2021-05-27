428210f9629d22d1d40c65ed36af47a1aee8a378
Reynold Figueroa <reynold0801@outlook.com>
2021-05-26 21:15:24 -0400

    Builder / Controls / Price

---
+++ src/components/Burger/Burger.js
@@ -0,0 +1,30 @@
+import React from "react";
+
+import "./Burger.css";
+import BurgerIngredient from "./BurgerIngredient/BurgerIngredient";
+
+const burger = (props) => {
+  let ingredients = Object.keys(props.ingredients)
+    .map((ingredient) => {
+      return [...Array(props.ingredients[ingredient])].map((_, i) => {
+        return <BurgerIngredient key={ingredient + i} type={ingredient} />;
+      });
+    })
+    .reduce((arr, el) => {
+      return arr.concat(el);
+    }, []);
+
+  if (ingredients.length === 0) {
+    ingredients = <p>Please start adding ingredients!</p>;
+  }
+
+  return (
+    <div className="Burger">
+      <BurgerIngredient type="bread-top" />
+      {ingredients}
+      <BurgerIngredient type="bread-bottom" />
+    </div>
+  );
+};
+
+export default burger;
